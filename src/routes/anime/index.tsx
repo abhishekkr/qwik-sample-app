b9495316d2fd507bc09c4434490a12378187d688
AbhishekKr <abhikumar163@gmail.com>
2023-05-28 20:32:40 +0530

    add quotes api integration to /anime & display random quote
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- src/routes/anime/index.tsx
+++ src/routes/anime/index.tsx
@@ -1,5 +1,27 @@
 import { component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+
+export const useAnimeQuote = routeLoader$(async () => {
+  const response = await fetch('https://animechan.vercel.app/api/random', {
+    headers: {Accept: 'application/json'},
+  });
+  return (await response.json()) as {
+    anime: string;
+    character: string;
+    quote: string;
+  };
+});
 
 export default component$(() => {
-  return <section class="section bright">Anime Facts</section>;
+  const animeQuote = useAnimeQuote();
+  return (
+    <section class="section bright">
+      <h3>Anime Quote</h3><br/>
+      <b>{animeQuote.value.quote}</b><br/>
+      <p>
+          <i>"{animeQuote.value.character}"</i> in <i>"{animeQuote.value.anime}"</i>
+      </p>
+    </section>
+  );
 });
